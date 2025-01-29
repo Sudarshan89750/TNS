6df7b6842ba7b360afa86bf622aac101a9947a25
Sudarshan89750 <sudarshanpawar6425@gmail.com>
2025-01-29 14:59:53 +0530

    Data Type and variable

--- TNS_JAVA/src/com/tns/java/Basics.java
+++ TNS_JAVA/src/com/tns/java/Basics.java
@@ -2,9 +2,20 @@ package com.tns.java;
 
 public class Basics {
 	public static void main(String[] args) {
-		int a=1;
-		int b=2;
-		System.out.print(a+b==3);
+		int a = 1;
+		int b = 2;
+		System.out.print(a + b == 3);
+		byte byteMax = 127;
+		byte byteMin = -128;
+
+		short shortMax = 32767;
+		short shortMin = -32768;
+
+		int intMax = 2147483647;
+		int intMin = -2147483647;
+		
+		long longMax=9223372036854775807L;
+		long longMin=-9223372036854775808L;
 	}
 
 }
