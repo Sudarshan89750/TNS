46ad9660f319c16547a1ce6ef11c0ccdc8ac5d3f
Sudarshan89750 <sudarshanpawar6425@gmail.com>
2025-01-30 16:01:06 +0530

    if elseif else

---
+++ TNS_JAVA/src/com/tns/java/ControlFlow.java
@@ -0,0 +1,24 @@
+package com.tns.java;
+
+public class ControlFlow {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+//		if (a > b) {
+//			System.out.println("a is greater that b");
+//		} else {
+//			System.out.println("b is greater than a");
+//		}
+//		
+//	
+		if (a == 5) {
+			System.out.println("a=5");
+		} else if (a > 5) {
+			System.out.println("a is greater that 5");
+		} else {
+			System.out.println("a is less than 5");
+		}
+
+	}
+
+}
